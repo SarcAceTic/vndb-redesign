e794e0a019223499ec5c7486003a0d682d38716c
SarcAceTic <sarcacetic@gmail.com>
2023-12-01 16:18:32 +0700

    Changed header color and removed some v-cols to optimize clean code

--- src/components/LandingHeader.vue
+++ src/components/LandingHeader.vue
@@ -6,48 +6,46 @@ const profileMenu = [
  { name: 'Profile', href: 'blank' },
  { name: 'Your List', href: 'blank' }
 ]
+
+//  let headerTrans = ref(true);
+
+//  const headerScroll = () => {
+//      headerTrans.value = false;
+//  }
 </script>
 
 <template>
  <header>
   <v-app class="bg-transparent h-0">
-   <v-app-bar class="bg-transparent" elevation="1">
-    <v-col
-     ><v-app-bar-title class="ml-10"><span>VNDB.org</span></v-app-bar-title>
-    </v-col>
+   <v-app-bar elevation="1">
+    <v-app-bar-title class="ml-10"><span>VNDB.org</span></v-app-bar-title>
     <v-col>
-     <v-btn class="ml-5 text-none w-50 justify-start border"
-      ><template v-slot:prepend><v-icon size="25">mdi-magnify</v-icon></template
-      >Search</v-btn
-     ></v-col
-    >
+     <v-btn class="ml-5 text-none w-50 justify-start border">
+      <template v-slot:prepend><v-icon size="25">mdi-magnify</v-icon></template>
+      Search
+     </v-btn>
+    </v-col>
     <template v-slot:append>
      <v-row class="align-center mr-5">
-      <v-col
-       ><v-btn icon href="https://www.patreon.com/vndb" target="_blank"
-        ><v-icon>mdi-patreon</v-icon></v-btn
-       >
-      </v-col>
+      <v-btn class="ma-3" icon href="https://www.patreon.com/vndb" target="_blank">
+       <v-icon>mdi-patreon</v-icon>
+      </v-btn>
       <v-divider class="border-opacity-25" vertical></v-divider>
-      <v-col><v-btn href="/" class="bg-transparent">Home</v-btn></v-col>
-      <v-col><v-btn class="bg-transparent">FAQ</v-btn></v-col>
-      <v-col><v-btn class="bg-transparent">API</v-btn></v-col>
+      <v-btn class="bg-transparent ma-3" href="/">Home</v-btn>
+      <v-btn class="bg-transparent ma-3">FAQ</v-btn>
+      <v-btn class="bg-transparent ma-3">API</v-btn>
       <v-divider class="border-opacity-25" vertical></v-divider>
-      <v-col>
-       <v-btn icon><v-icon>mdi-bell</v-icon></v-btn>
-      </v-col>
-      <v-col>
-       <v-avatar>
-        <v-img src="/pfpplaceholder.jpeg"></v-img>
-        <v-menu activator="parent">
-         <v-list>
-          <v-list-item v-for="menuItem in profileMenu">
-           <h4>{{ menuItem.name }}</h4>
-          </v-list-item>
-         </v-list>
-        </v-menu>
-       </v-avatar>
-      </v-col>
+      <v-btn class="ma-3" icon><v-icon>mdi-bell</v-icon></v-btn>
+      <v-avatar>
+       <v-img src="/pfp.jpg"></v-img>
+       <v-menu activator="parent">
+        <v-list>
+         <v-list-item v-for="menuItem in profileMenu">
+          <h4>{{ menuItem.name }}</h4>
+         </v-list-item>
+        </v-list>
+       </v-menu>
+      </v-avatar>
      </v-row>
     </template>
    </v-app-bar>
@@ -57,4 +55,8 @@ const profileMenu = [
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600;700&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+.some-aqua {
+ background-color: aqua !important;
+}
 </style>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -23,66 +23,72 @@ console.log(somevn)
   <div class="landing-image h-screen">
    <v-container class="fill-height">
     <v-row class="unlayered align-center" no-gutters>
-     <v-col cols="6"
-      ><v-col
-       ><v-col cols=""><h1 class="">Visual Novel Database</h1></v-col>
-       <v-col cols=""
-        ><p class="">
+     <v-col cols="6">
+      <v-col>
+       <v-col cols=""><h1 class="">Visual Novel Database</h1></v-col>
+       <v-col cols="">
+        <p class="">
          VNDB.org strives to be a comprehensive database for information about visual novels.
          website is built as a wiki, meaning that anyone can freely add and contribute information
          to the database, allowing us to create the largest, most accurate and most up-to-date
          visual novel database on the web.
-        </p></v-col
-       >
-       <v-col
-        ><v-btn class="mr-5" color="blue-darken-1" elevation="10"
-         >See Visual Novels<template v-slot:append
-          ><v-icon>mdi-arrow-right</v-icon></template
-         ></v-btn
-        ><v-btn variant="outlined">Sign Up</v-btn></v-col
-       ></v-col
-      ></v-col
-     >
-     <v-col cols="6"
-      ><v-col cols="12" class="text-center vn-data"
-       ><h3>
-        <v-icon color="green">mdi-circle-small</v-icon>{{ somevn.data.vn.toLocaleString() }}
+        </p>
+       </v-col>
+       <v-col>
+        <v-btn class="mr-5" color="blue-darken-1" elevation="10">
+         See Visual Novels
+         <template v-slot:append><v-icon>mdi-arrow-right</v-icon></template>
+        </v-btn>
+        <v-btn variant="outlined">Sign Up</v-btn>
+       </v-col>
+      </v-col>
+     </v-col>
+     <v-col cols="6">
+      <v-col cols="12" class="text-center vn-data">
+       <h3>
+        <v-icon color="green">mdi-circle-small</v-icon>
+        {{ somevn.data.vn.toLocaleString() }}
        </h3>
-       <h5>Visual Novels</h5></v-col
-      ></v-col
-     >
-     <v-col cols="12" class="mt-10"
-      ><v-row class="other-data"
-       ><v-col cols="" class="text-center"
-        ><h3>
-         <v-icon color="green">mdi-circle-small</v-icon>{{ somevn.data.chars.toLocaleString() }}
+       <h5>Visual Novels</h5>
+      </v-col>
+     </v-col>
+     <v-col cols="12" class="mt-10">
+      <v-row class="other-data">
+       <v-col cols="" class="text-center">
+        <h3>
+         <v-icon color="green">mdi-circle-small</v-icon>
+         {{ somevn.data.chars.toLocaleString() }}
         </h3>
-        <h5>Characters</h5></v-col
-       >
-       <v-col cols="" class="text-center"
-        ><h3>
-         <v-icon color="green">mdi-circle-small</v-icon>{{ somevn.data.producers.toLocaleString() }}
+        <h5>Characters</h5>
+       </v-col>
+       <v-col cols="" class="text-center">
+        <h3>
+         <v-icon color="green">mdi-circle-small</v-icon>
+         {{ somevn.data.producers.toLocaleString() }}
         </h3>
-        <h5>Producers</h5></v-col
-       >
-       <v-col cols="" class="text-center"
-        ><h3>
-         <v-icon color="green">mdi-circle-small</v-icon>{{ somevn.data.releases.toLocaleString() }}
+        <h5>Producers</h5>
+       </v-col>
+       <v-col cols="" class="text-center">
+        <h3>
+         <v-icon color="green">mdi-circle-small</v-icon>
+         {{ somevn.data.releases.toLocaleString() }}
         </h3>
-        <h5>Releases</h5></v-col
-       >
-       <v-col cols="" class="text-center"
-        ><h3>
-         <v-icon color="green">mdi-circle-small</v-icon>{{ somevn.data.tags.toLocaleString() }}
+        <h5>Releases</h5>
+       </v-col>
+       <v-col cols="" class="text-center">
+        <h3>
+         <v-icon color="green">mdi-circle-small</v-icon>
+         {{ somevn.data.tags.toLocaleString() }}
         </h3>
-        <h5>Tags</h5></v-col
-       >
-       <v-col cols="" class="text-center"
-        ><h3>
-         <v-icon color="green">mdi-circle-small</v-icon>{{ somevn.data.traits.toLocaleString() }}
+        <h5>Tags</h5>
+       </v-col>
+       <v-col cols="" class="text-center">
+        <h3>
+         <v-icon color="green">mdi-circle-small</v-icon>
+         {{ somevn.data.traits.toLocaleString() }}
         </h3>
-        <h5>Traits</h5></v-col
-       >
+        <h5>Traits</h5>
+       </v-col>
       </v-row>
      </v-col>
     </v-row>
