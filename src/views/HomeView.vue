b8ba044b20796bab1981e543f5578a2f8455dfed
SarcAceTic <sarcacetic@gmail.com>
2023-12-01 16:19:05 +0700

    Removed some margin

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script setup>
 import LandingPage from '../components/LandingPage.vue';
+import LandingVN from '../components/LandingVN.vue';
+import { ref, onMounted } from 'vue'
 </script>
 
 <template>
   <Suspense>
     <LandingPage />
   </Suspense>
-  
+  <Suspense>
+    <LandingVN class="mt-8" />
+  </Suspense>
 </template>
